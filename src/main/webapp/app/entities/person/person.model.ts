3c94d9089a86f41b3c0f177eaf051afe2279960f
0Chris0 <ziegner.christian@gmail.com>
2017-11-22 18:56:26 +0100

    create person entity stuff

---
+++ src/main/webapp/app/entities/person/person.model.ts
@@ -0,0 +1,14 @@
+import { BaseEntity } from './../../shared';
+
+export class Person implements BaseEntity {
+    constructor(
+        public id?: string,
+        public name?: string,
+        public surname?: string,
+        public location?: string,
+        public grade?: string,
+        public projectAvailability?: number,
+        public ahcr?: number,
+    ) {
+    }
+}
