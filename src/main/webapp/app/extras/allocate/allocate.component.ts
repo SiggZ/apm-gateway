2b13660f4848cd8e01ea0534a23facd1ba06052b
Milena Makaveeva <ga92yup@mytum.de>
2017-12-17 15:55:34 +0100

    allcation code

--- src/main/webapp/app/extras/allocate/allocate.component.ts
+++ src/main/webapp/app/extras/allocate/allocate.component.ts
@@ -1,36 +1,59 @@
 import {AfterViewInit, Component, OnInit, ViewChild, ElementRef} from '@angular/core';
 import { jqxListBoxComponent } from 'jqwidgets-framework/jqwidgets-ts/angular_jqxlistbox';
+import {TeamService} from '../../entities/team/team.service';
+import {Team} from '../../entities/team/team.model';
+import { JhiEventManager, JhiAlertService } from 'ng-jhipster';
+import { ITEMS_PER_PAGE, Principal, ResponseWrapper } from '../../shared';
 
 @Component({
     selector: 'jhi-allocate',
     templateUrl: './allocate.component.html'
 })
 export class AllocateComponent implements AfterViewInit {
-@ViewChild('jqxListBox') myListBox: jqxListBoxComponent;
-@ViewChild('selectionLog') selectionLog: ElementRef;
-    selectedTeams: string[];
+    @ViewChild('jqxListBox') myListBox: jqxListBoxComponent;
+    @ViewChild('selectionLog') selectionLog: ElementRef;
+    teams: Team[] = new Array();
+    source: string[]= new Array();
+    dataAdapter: string;
+    constructor(
+        private teamService: TeamService,
+        private jhiAlertService: JhiAlertService,
+        private eventManager: JhiEventManager,
+        private principal: Principal
 
-    source: string[] =
-        [
-            'Team A',
-            'Team B',
-            'Team C',
-            'Team D',
-            'Team E',
-            'Team F',
-            'Team G',
-            'Team H'
-        ];
-    dataAdapter: string = new jqx.dataAdapter(this.source);
-    displaySelectedItems() {
-        //  this.selectedTeams = this.myListBox.getSelectedItems();
-        console.log('SelectedItems clicked');
+    ) {
+        console.log("Constructor called");
+
+    }
+
+
+    initializeTeams() {
+        console.log("Initialize Teams called");
+        this.teamService.query().subscribe(
+            (res: ResponseWrapper) => {
+                this.teams = res.json;
+            },
+            (res: ResponseWrapper) => this.onError(res.json)
+        );
+
+        this.dataAdapter = new jqx.dataAdapter(this.teams);
     }
 
     ngAfterViewInit() {
-        this.myListBox.selectIndex(2);
-        this.myListBox.selectIndex(5);
-        this.myListBox.selectIndex(7);
+    }
+
+    ngOnInit(){
+        console.log('Initiated called');
+        this.initializeTeams();
+        /*        this.source = new Array(); */
+
+
+        console.log('Results returned'  + this.teams);
+
+    }
+
+    private onError(error) {
+        this.jhiAlertService.error(error.message, null, null);
     }
 
 }
