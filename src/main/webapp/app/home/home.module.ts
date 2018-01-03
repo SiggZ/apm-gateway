bb4f06e005673df0fdb5af886c3f8e299f6f9aec
Milena Makaveeva <ga92yup@mytum.de>
2018-01-04 00:03:41 +0100

    Adding the Material Design

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -6,11 +6,13 @@ import { GatewaySharedModule } from '../shared';
 import { HOME_ROUTE, HomeComponent, ALLOCATE_ROUTE  } from './';
 import {AllocateComponent} from '../extras/allocate/allocate.component';
 import { jqxListBoxComponent } from 'jqwidgets-framework/jqwidgets-ts/angular_jqxlistbox';
+import {MdButtonModule} from '@angular/material';
 
 @NgModule({
     imports: [
         GatewaySharedModule,
-        RouterModule.forRoot([ HOME_ROUTE, ALLOCATE_ROUTE], { useHash: true })
+        RouterModule.forRoot([ HOME_ROUTE, ALLOCATE_ROUTE], { useHash: true }),
+        MdButtonModule
     ],
     declarations: [
         HomeComponent,
