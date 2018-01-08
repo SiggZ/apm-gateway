7069219536d2331a1856c4cb6456c8eff02cd493
Milena Makaveeva <ga92yup@mytum.de>
2018-01-08 21:48:27 +0100

    Allocation screen ready for integration

--- src/main/webapp/app/extras/allocate/allocate.component.ts
+++ src/main/webapp/app/extras/allocate/allocate.component.ts
@@ -13,13 +13,14 @@ import { Subscription } from 'rxjs/Rx';
     selector: 'jhi-allocate',
     templateUrl: './allocate.component.html'
 })
-export class AllocateComponent implements AfterViewInit, OnInit, OnDestroy {
+export class AllocateComponent implements OnInit, OnDestroy {
     eventSubscriber: Subscription;
     selectedSprint: Iteration;
     selectedTeams: Array<any>;
     teams: Array<Team>;
     iterations: Array<Iteration>;
-    sprintFormGroup: FormGroup;
+    sprintControl: FormControl;
+    teamSelectionControl: FormControl;
 
     constructor(
         private teamService: TeamService,
@@ -35,19 +36,24 @@ export class AllocateComponent implements AfterViewInit, OnInit, OnDestroy {
         this.initializeTeams();
         this.initializeIterations();
         this.registerChangeInTeams();
-
-        this.sprintFormGroup = new FormGroup({ sprintDropdownControl: new FormControl() });
+        this.sprintControl = new FormControl();
+        this.sprintControl.valueChanges.subscribe((event: any) => {
+            console.log('Sprint Selection made');
+            this.initializeTeamsForSprint();
+        });
+        this.teamSelectionControl = new FormControl();
+        this.teamSelectionControl.valueChanges.subscribe((event: any) => {
+            console.log('Team Selection made');
+        });
     };
     ngOnDestroy() {
         this.eventManager.destroy(this.eventSubscriber);
     }
-    ngAfterViewInit(): void {
-    /*    this.teamListBox.createComponent(this.options);
-        this.teamListBox.onChange.subscribe(
-            (event: Event) => this.onListBoxChange(this, event)
-        )*/
-    };
 
+    initializeTeamsForSprint() {
+        console.log('Change that later');
+        this.initializeTeams();
+    }
     initializeTeams(): void {
         this.teamService.query().subscribe(
             (res: ResponseWrapper) => this.onInitTeamsSuccess(res.json),
@@ -76,10 +82,9 @@ export class AllocateComponent implements AfterViewInit, OnInit, OnDestroy {
     clearSelectedTeams(): void {
         this.selectedTeams = new Array<any>();
     };
-  /*  displaySelectedTeams(): void {
-        this.selectedTeams = this.teamListBox.getSelectedItems();
-    };*/
-
+    displaySelectedTeams(): void {
+      //  this.selectedTeams = this.teamListBox.getSelectedItems();
+    };
 
  /*   onListBoxChange(context: AllocateComponent, event: any): void {
         context.displaySelectedTeams();
